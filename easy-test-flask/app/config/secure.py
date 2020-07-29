0ebb27d24353bfd236e89b4c12fedd6989b4eefc
guojiaxing <302802003@qq.com>
2020-07-29 20:10:12 +0800

    secure.py

--- easy-test-flask/app/config/secure.py
+++ easy-test-flask/app/config/secure.py
@@ -13,40 +13,29 @@ class DevelopmentSecure(BaseConfig):
     """
     开发环境安全性配置
     """
-    SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:root@ip:3306/easy-test'
+    SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:root@www.guojiaxing.red:8081/easy-test'
 
     SQLALCHEMY_ECHO = False
 
     SECRET_KEY = '\x88W\xf09\x91\x07\x98\x85\x87\x96\xb0A\xc68\xf9\xecJJU\x12\xc5V\xbe\x8b\xef\xd7\xd8\xd3\xe6\x95*6'
 
-    MONGO_URI = 'mongodb://root:root@ip:27017/easyTest?authSource=admin'
+    MONGO_URI = 'mongodb://root:mongo2020@www.guojiaxing.red:8090/easyTest?authSource=admin'
 
-    CELERY_BROKER_URL = 'amqp://admin:admin@ip:8083/my_vhost'
+    CELERY_BROKER_URL = 'amqp://admin:ftlb2000@www.guojiaxing.red:8083/my_vhost'
 
     # scheduler
     SCHEDULER_JOBSTORES = {
-        'default': MongoDBJobStore(host='ip', port=27017, username='root', password='root')
+        'default': MongoDBJobStore(host='www.guojiaxing.red', port=8090, username='root', password='mongo2020')
     }
-
-    SCHEDULER_EXECUTORS = {
-        'default': {'type': 'threadpool', 'max_workers': 20}
-    }
-    SCHEDULER_JOB_DEFAULTS = {
-        'coalesce': True,  # 累计的 任务是否执行。True不执行，False,执行
-        'max_instances': 30,  # 同一个任务在线程池中最多跑的实例数
-        'misfire_grace_time': 600  # 超过用户设定的时间范围外，该任务依旧执行的时间(单位时间s)
-    }
-
-    SCHEDULER_API_ENABLED = True
 
     # mail
     MAIL_SERVER = 'smtp.sina.cn'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_DEFAULT_SENDER = ''
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
+    MAIL_DEFAULT_SENDER = '15234093915@sina.cn'
+    MAIL_USERNAME = '15234093915@sina.cn'
+    MAIL_PASSWORD = '1353a7a662f4e7fb'
 
     # mock server
     MOCK_SERVER = 'http://127.0.0.1:5000'
@@ -75,29 +64,18 @@ class ProductionSecure(BaseConfig):
         'default': MongoDBJobStore(host='mongo', port=27017, username='root', password='mongo2020')
     }
 
-    SCHEDULER_EXECUTORS = {
-        'default': {'type': 'threadpool', 'max_workers': 20}
-    }
-    SCHEDULER_JOB_DEFAULTS = {
-        'coalesce': True,  # 累计的 任务是否执行。True不执行，False,执行
-        'max_instances': 30,  # 同一个任务在线程池中最多跑的实例数
-        'misfire_grace_time': 600  # 超过用户设定的时间范围外，该任务依旧执行的时间(单位时间s)
-    }
-
-    SCHEDULER_API_ENABLED = True
-
     # mail
     MAIL_SERVER = 'smtp.sina.cn'
     MAIL_PORT = 465
     MAIL_USE_TLS = False
     MAIL_USE_SSL = True
-    MAIL_DEFAULT_SENDER = ''
-    MAIL_USERNAME = ''
-    MAIL_PASSWORD = ''
+    MAIL_DEFAULT_SENDER = '15234093915@sina.cn'
+    MAIL_USERNAME = '15234093915@sina.cn'
+    MAIL_PASSWORD = '1353a7a662f4e7fb'
 
     # mock server
     MOCK_SERVER = 'http://api:5000'
 
     API_SERVER = 'http://api:5000'
 
-    SITE_DOMAIN = ''
+    SITE_DOMAIN = 'https://www.guojiaxing.red'
